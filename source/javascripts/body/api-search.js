95b734bc2778a5629882b942d7a5f712f504fa04
Javier Artero <javier.artero@gmail.com>
2018-05-28 01:13:39 +0200

    fixes api

--- source/javascripts/body/api-search.js
+++ source/javascripts/body/api-search.js
@@ -12,38 +12,13 @@
         .find('.search-by-name')
         .html();
       const filter = $(input).val();
+      const $elements = $(element).add($(element).next());
 
       if (string.indexOf(filter) > -1) {
-        console.log(string);
-        $(element)
-          .show()
-          .next()
-          .show();
+        $elements.show();
       } else {
-        $(element)
-          .hide()
-          .next()
-          .hide();
+        $elements.hide();
       }
     });
-  }
-
-  function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById('myUL');
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName('a')[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = '';
-      } else {
-        li[i].style.display = 'none';
-      }
-    }
   }
 })();
