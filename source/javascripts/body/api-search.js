f023c857e22c9188bf12e7ebe67f97857e895cbc
Javier Artero <javier.artero@gmail.com>
2018-05-29 00:41:45 +0200

    look and feel api search and fixes

--- source/javascripts/body/api-search.js
+++ source/javascripts/body/api-search.js
@@ -1,23 +1,50 @@
 (function() {
   'use strict';
 
-  const input = '.api-search input';
-  const tableItem = '.table-collapse__btn';
+  const input = '.api-search input',
+    tableItem = '.api-content .table-collapse__btn',
+    checkboxColumns = '.api-content th .checkbox input';
 
   $(input).on('keyup', searchTable);
+  $(checkboxColumns).on('change', setPlaceholder);
+
+  function setPlaceholder() {
+    let searchText = '';
+
+    $(checkboxColumns).each(function() {
+      if ($(this).is(':checked')) {
+        const placeholder = $(this).data('placeholder');
+
+        searchText +=
+          searchText === '' ? `search by ${placeholder}` : ` and ${placeholder}`;
+      }
+    });
+
+    $(input)
+      .prop('disabled', searchText === '')
+      .attr('placeholder', searchText);
+
+    searchTable();
+  }
 
   function searchTable() {
-    $(tableItem).each((index, element) => {
-      const string = $(element)
-        .find('.search-by-name')
-        .html();
+    $(tableItem).each(function() {
       const filter = $(input).val();
-      const $elements = $(element).add($(element).next());
+      const $this = $(this);
+      const $elements = $(this).add($(this).next());
+
+      let string = '';
+
+      $(checkboxColumns).each(function() {
+        if ($(this).is(':checked')) {
+          string += $this.find($(this).data('target')).text();
+        }
+      });
 
       if (string.indexOf(filter) > -1) {
-        $elements.show();
+        $(this).show();
       } else {
-        $elements.hide();
+        $(this).hide();
       }
     });
   }
